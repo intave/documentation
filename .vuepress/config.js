f589d416dd0952804acdddcd90fceef0e9bf92f3
Richard <strunk876@gmail.com>
2021-09-15 15:39:24 +0200

    Add first stetch

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
     title: "Intave",
-    description: "Documentation around the Intave anticheat"
+    description: "Documentation around the Intave anticheat",
+    themeConfig: {
+        nav: [
+            {text: "Front", link: '/'},
+            {text: "Mechanic", link: '/mechanic'},
+            {text: "Guide", link: '/guide'},
+        ]
+    },
+    sidebar: 'auto'
 } 
---
+++ README.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroText: Intave
+tagline: Documentation, guides and mechanics
+actionText: Setup
+actionLink: /guide/
+footer: Edit this page on https://github.com/intave | CC BY 4.0
+---
